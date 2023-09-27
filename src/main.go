e9c0f90b2db77d10f4eae3b77c29b84a94953678
Duah Kwadwo Adjei <duah14@outlook.com>
2023-09-27 19:43:54 +0000

    [+] Wed Sep 27 07:43:54 PM GMT 2023

--- src/main.go
+++ src/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/Android-Jester/bank_queue_backend_go/src/interface/analytics"
 	"github.com/Android-Jester/bank_queue_backend_go/src/interface/client"
 	"github.com/Android-Jester/bank_queue_backend_go/src/interface/server"
 	"github.com/gin-gonic/gin"
 )
-
-var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
@@ -22,62 +18,11 @@ func setupRouter() *gin.Engine {
 	analytic := router.Group("/analytics")
 	analytics.Routes(analytic)
 
-	// Ping test
-	router.GET("/ping", func(context *gin.Context) {
-		context.String(http.StatusOK, "pong")
-	})
-
-	// Get user value
-	router.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := router.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
-
 	return router
 }
 
 func main() {
 	r := setupRouter()
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	r.Run(":3000")
 }
